1d06f6bbae91f8b4a35698af3475e1052d777f51
TANAY TANDON <tanay1400089@gmail.com>
2015-09-11 21:21:53 +0530

    fuck everyone

--- contag/settings.py
+++ contag/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'contag',
-        'HOST': 'contagnew.c1mliw98jdpx.ap-southeast-1.rds.amazonaws.com',
+        'HOST': 'contag.c1mliw98jdpx.ap-southeast-1.rds.amazonaws.com',
         'PORT': '3306',
         'USER': 'contag',
         'PASSWORD': 'Contag101',

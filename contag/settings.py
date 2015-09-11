99f63f6ef587601809db6b78e06d5310ec9a41c1
TANAY TANDON <tanay1400089@gmail.com>
2015-09-11 19:49:29 +0530

    modified host name

--- contag/settings.py
+++ contag/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'contag',
-        'HOST': 'contagdbinstance.ccciwwjzjumq.us-west-2.rds.amazonaws.com',
+        'HOST': 'contag.c1mliw98jdpx.ap-southeast-1.rds.amazonaws.com:3306',
         'PORT': '3306',
         'USER': 'contag',
         'PASSWORD': 'Contag101',

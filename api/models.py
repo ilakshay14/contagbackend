2f0acb9f44272244979c5bfe0b026b93452f2e1e
TANAY TANDON <tanay1400089@gmail.com>
2015-09-12 14:48:25 +0530

    added requirements.txt

--- api/models.py
+++ api/models.py
@@ -178,5 +178,6 @@ class OTPToken(TimeStampedModel):
         otp_message = "Dear user you One Time Password(OTP) for login to Contag is " + str(self.otp) + "."
         print(otp_message)
         sms = SMS()
-        if self.number == 9971528807:
-            sms.send(self.number, otp_message)
+        # if self.number == 9971528807:
+        #     sms.send(self.number, otp_message)
+        sms.send(self.number, otp_message)
